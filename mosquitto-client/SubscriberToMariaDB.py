4e4520614030435f7a6a963a9a50d172dd5ce1de
Cristian F Ritter <cristianritter@gmail.com>
2020-10-19 20:44:13 -0300

    Adicionado cliente MQTT to MariaDb

---
+++ mosquitto-client/SubscriberToMariaDB.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='admin', password='cristian86',
+                              host='localhost',
+                              database='energymeter')
+
+cursor=cnx.cursor()
+add_data = "INSERT INTO data (id, timecode, tensao, corrente) VALUES(%s, %s, %s, %s)"
+data = (0, 3333333, 220, 20)
+
+cursor.execute(add_data, data)
+cnx.commit()
+cursor.close()
+cnx.close()
