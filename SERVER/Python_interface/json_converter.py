1bc96fc2bd1cf2162fa695c840bc9000f027e830
Cristian F Ritter <cristianritter@gmail.com>
2020-10-29 22:33:49 -0300

    criacao da secao mqtt_receiver.py

--- SERVER/Python_interface/json_converter.py
+++ SERVER/Python_interface/json_converter.py
@@ -1,7 +1,20 @@
 import mqtt_receiver
 
-mqtt_receiver.create_conections()
+def do_this(msg):
+    print(msg)
+
+def on_message(client, userdata, msg):
+    """The callback for when a PUBLISH message is received from the server."""
+    #print(msg.topic + ' ' + str(msg.payload))
+    mensagem_recebida = str(msg.payload.decode("utf-8","ignore"))
+    do_this(mensagem_recebida)
+
+Received = mqtt_receiver.MQTTConn(on_message)
+Received.create_connections()
 
 
-#def received_mqtt():
-#    print("received")
+
+
+
+
+
--- SERVER/Python_interface/mqtt_receiver.py
+++ SERVER/Python_interface/mqtt_receiver.py
@@ -1,32 +1,44 @@
 import paho.mqtt.client as mqtt
-#import json_converter
-import config
+import parse_config as parse
+
+configuration = parse.ConfPacket()
+configs = configuration.load_config()    
 
 def on_connect(client, userdata, flags, rc):
     """ The callback for when the client receives a CONNACK response from the server."""
     print('Connected with result code ' + str(rc))
-    for topic_address in config.items['MQTT']['TOPICS']:
-        topic = config.items['MQTT']['TOPICS'][topic_address]
+    for topic_address in configs['MQTT']['TOPICS']:
+        topic = configs['MQTT']['TOPICS'][topic_address]
         client.subscribe(topic)
 
-def on_message(client, userdata, msg):
+class MQTTConn:
+    def __init__(self, on_message):
+        #self.variable = value
+        self.on_message = on_message
+        pass
+
+    def create_connections(self):
+        #print(configs)
+        client = mqtt.Client()
+        host = configs['MQTT']['HOST_ADDRESS']
+        user = configs['MQTT']['USER']
+        passwd = configs['MQTT']['PASSWORD']
+        client.username_pw_set(user, passwd)
+        client.on_connect = on_connect
+        client.on_message = self.on_message
+        client.connect(host, 1883)
+        client.loop_forever()
+    
+def on_message_f(client, userdata, msg):
     """The callback for when a PUBLISH message is received from the server."""
     #print(msg.topic + ' ' + str(msg.payload))
     mensagem_b = str(msg.payload.decode("utf-8","ignore"))
     print(mensagem_b)
 
-def create_conections():
-    client = mqtt.Client()
-    host = config.items['MQTT']['HOST_ADDRESS']
-    user = config.items['MQTT']['USER']
-    passwd = config.items['MQTT']['PASSWORD']
-    client.username_pw_set(user, passwd)
-    client.on_connect = on_connect
-    client.on_message = on_message
-    client.connect(host, 1883)
-    client.loop_forever()
 
 def main():
-    create_conections()
+    Received = MQTTConn(on_message_f)
+    Received.create_connections()
+
 if __name__ == '__main__':
     main()
